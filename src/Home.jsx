e2c0008dc154e3782fc9d4838eb90bbd90da927a
Isabella Chen <143982520+isabellachen1@users.noreply.github.com>
2025-02-03 16:25:02 +0000

    classwork

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,12 @@
+import Gallery from "./components/Gallery.jsx"
+
 export default function Home() {
+  return(
   <main>
-    <Gallery>
+    <Gallery/>
 
       <h2>Rendered Date</h2>
       <p>{new Date().toLocaleString()}</p>
   </main>
+  )
 }
